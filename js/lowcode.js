009bec71ac37681ccea3b7a35727cedebfbe76c1
GuztLimz <gustavolima.oli@hotmail.com>
2021-07-23 14:47:01 -0300

    Grandes alterações

--- js/lowcode.js
+++ js/lowcode.js
@@ -1,19 +1,19 @@
-function verifyReq(){   
+function clipBoard(){
+    var text = document.getElementById("resultImpresso").value;
+    navigator.clipboard.writeText(text);
+}
+
+function codeGenerator(){   
     
     //Pegando os valores do HTML
     let componentsUser = []
-    componentsUser[0] = document.getElementById("vlrId").value;
-    componentsUser[1] = document.getElementById("vlrClass").value;
-    componentsUser[2] = document.getElementById("vlrName").value;
-    componentsUser[3] = document.getElementById("vlrValue").value;
+        componentsUser[0] = document.getElementById("vlrClass").value;
 
     //Colocando os valores do HTML junto com uma String
     let components = [
-        `id="${componentsUser[0]}"`,
-        `class="${componentsUser[1]}"`,
-        `name="${componentsUser[2]}"`,
-        `value="${componentsUser[3]}"`
+        `class="${componentsUser[0]}"`
     ]
+        
 
     //Conteúdo do elemento (Título do botão por exemplo)
     let content = document.getElementById("vlrContent").value;
@@ -24,7 +24,7 @@ function verifyReq(){
     }
 
     //Filtrando o Array dos valores e removendo dados sem valor
-    let newComponentsFilter = components.filter(function(value){
+/*     let newComponentsFilter = components.filter(function(value){
 
         result = value.indexOf('""') > -1 
         if(result != true){ 
@@ -32,35 +32,51 @@ function verifyReq(){
         }
         else{
         }
-    })
+    }) */
 
     //Trocando os separadores padrões do Array(,)
-    newComponentsFilter = newComponentsFilter.join(" ")
+/*     newComponentsFilter = newComponentsFilter.join(" ") */
 
     //Array de Comandos
-    let Content = [
-        `<button ${newComponentsFilter}>${content}</button>`,
-        `<p ${newComponentsFilter}>${content}</p>`
+    let command = [
+        `<button ${components}>${content}</button>`,
     ]
 
 
     //Buscando a requisição do Usuário e inserindo em uma variável
-    let reqUser = document.getElementById("commandLine").value
+    let reqUser = "botão";
     let resultReference
 
     //Buscando a requisição do usuário e apresentando os respectivos elementos
     switch(reqUser){
 
         case "botão":
-            document.getElementById("resultImpresso").value = Content[0];
-            resultReference = Content[0];
-            break;
-        case "parágrafo":
-            document.getElementById("resultImpresso").value = Content[1];
-            resultReference = Content[1];
+            //document.getElementById("resultImpresso").value = command[0];
+            resultReference = command[0];
             break;
         default:
             document.getElementById("resultImpresso").value = "Dados Inválidos";
     }
-    document.getElementById("resultInterpretado").innerHTML = resultReference;
+    //document.getElementById("buttonResult").innerHTML = resultReference;
+
+    //BackgroundColor
+    let backcolor = document.getElementById("backColorPick").value;
+    document.getElementById("backColorValue").innerHTML = backcolor;
+
+    //Color
+    let colorVl = document.getElementById("colorPick").value;
+    document.getElementById("colorValue").innerHTML = colorVl;
+
+    document.getElementById("buttonResult").style.backgroundColor = backcolor
+    document.getElementById("buttonResult").style.color = colorVl
+}
+
+function colors(){
+    //BackgroundColor
+    let backcolor = document.getElementById("backColorPick").value;
+    document.getElementById("backColorValue").innerHTML = backcolor;
+
+    //Color
+    let colorVl = document.getElementById("colorPick").value;
+    document.getElementById("colorValue").innerHTML = colorVl;
 }
