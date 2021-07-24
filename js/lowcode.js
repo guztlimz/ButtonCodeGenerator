5623c743b935f188f695da5df55c0d579906d25f
GuztLimz <gustavolima.oli@hotmail.com>
2021-07-24 14:48:10 -0300

    Responsividade e Display Terminados

--- js/lowcode.js
+++ js/lowcode.js
@@ -1,63 +1,16 @@
+//copy text
 function clipBoard(){
     var text = document.getElementById("resultImpresso").value;
     navigator.clipboard.writeText(text);
 }
 
-function codeGenerator(){   
-    
-    //Pegando os valores do HTML
-    let componentsUser = []
-        componentsUser[0] = document.getElementById("vlrClass").value;
-
-    //Colocando os valores do HTML junto com uma String
-    let components = [
-        `class="${componentsUser[0]}"`
-    ]
-        
+//Settings
+function codeSettings(){   
 
     //Conteúdo do elemento (Título do botão por exemplo)
     let content = document.getElementById("vlrContent").value;
 
-    //Se o usuário não colocar um Conteúdo do elemento...
-    if(content == ""){
-        content="Template"
-    }
-
-    //Filtrando o Array dos valores e removendo dados sem valor
-/*     let newComponentsFilter = components.filter(function(value){
-
-        result = value.indexOf('""') > -1 
-        if(result != true){ 
-            return value
-        }
-        else{
-        }
-    }) */
-
-    //Trocando os separadores padrões do Array(,)
-/*     newComponentsFilter = newComponentsFilter.join(" ") */
-
-    //Array de Comandos
-    let command = [
-        `<button ${components}>${content}</button>`,
-    ]
-
-
-    //Buscando a requisição do Usuário e inserindo em uma variável
-    let reqUser = "botão";
-    let resultReference
-
-    //Buscando a requisição do usuário e apresentando os respectivos elementos
-    switch(reqUser){
-
-        case "botão":
-            //document.getElementById("resultImpresso").value = command[0];
-            resultReference = command[0];
-            break;
-        default:
-            document.getElementById("resultImpresso").value = "Dados Inválidos";
-    }
-    //document.getElementById("buttonResult").innerHTML = resultReference;
+    document.getElementById("buttonResult").innerHTML = content;
 
     //BackgroundColor
     let backcolor = document.getElementById("backColorPick").value;
@@ -69,8 +22,47 @@ function codeGenerator(){
 
     document.getElementById("buttonResult").style.backgroundColor = backcolor
     document.getElementById("buttonResult").style.color = colorVl
+
+    //Border
+    let vlrBorder = document.getElementById("vlrBorder").value;
+
+    let bordercolor = document.getElementById("borderColorPick").value;
+    document.getElementById("borderColorValue").innerHTML = bordercolor;
+
+    let borderStyle = document.getElementById("borderStyle").value;
+    
+    if(vlrBorder != 0){
+        document.getElementById("buttonResult").style.border = vlrBorder+"px " + borderStyle + " " + bordercolor
+    }
+    else{
+        
+    }
+
+
+    //Height
+
+    let vlrHeight = document.getElementById("vlrHeight").value
+
+    if(vlrHeight != 0){
+        document.getElementById("buttonResult").style.height = vlrHeight+"px"
+    }
+    else{
+        document.getElementById("buttonResult").style.height = "50px"
+    }
+
+    //Width
+
+    let vlrWidth = document.getElementById("vlrWidth").value
+
+    if(vlrWidth!=0){
+        document.getElementById("buttonResult").style.width = vlrWidth+"px"
+    }
+    else{
+        document.getElementById("buttonResult").style.width = "150px"
+    }
 }
 
+//Colors definitions
 function colors(){
     //BackgroundColor
     let backcolor = document.getElementById("backColorPick").value;
